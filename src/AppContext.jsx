cb51f40d341dacea3c97106b0a31599242112c21
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-26 00:58:42 +0200

    appContext.jsx added

---
+++ src/AppContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const AppContext = createContext();
+
+const bookUrl = "https://edwardtanguay.netlify.app/share/books.json";
+const memberUrl = "https://edwardtanguay.netlify.app/share/employees.json";
+
+export const AppProvider = ({ children }) => {
+  const [books, setBooks] = useState([]);
+  const [members, setMembers] = useState([]);
+  const siteTitle = "The Online Book Club";
+
+  useEffect(() => {
+    (async () => {
+      setBooks((await axios.get(bookUrl)).data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      setMembers((await axios.get(memberUrl)).data);
+    })();
+  }, []);
+
+  const handleDelete = (book) => {
+    const _books = books.filter((m) => m.id !== book.id);
+    setBooks(_books);
+  };
+
+  const handleEdit = (book) => {
+    book.title = book.title + " - FINISHED";
+    setBooks([...books]);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        siteTitle,
+        books,
+        setBooks,
+        members,
+        handleDelete,
+        handleEdit,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
