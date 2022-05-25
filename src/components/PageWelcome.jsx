a6db872d3b72462ff1af535dd8869e962c317d97
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-25 09:08:17 +0200

    PageWelcome.jsx added

---
+++ src/components/PageWelcome.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
+export const PageWelcome = () => {
+  const { siteTitle, books, members } = useContext(AppContext);
+
+  return (
+    <div className="page_welcome">
+      <h2>Welcome</h2>
+      <p>Welcome to {siteTitle}.</p>
+      <p>
+        We have {members.length} members in our club and we have read{" "}
+        {books.length} books.
+      </p>
+    </div>
+  );
+};
